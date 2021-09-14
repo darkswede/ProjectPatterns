d7010f17c14478678e89b81e143325f980e0935a
Marcin Pietrucha <mpietrucha@g2a.com>
2021-09-14 19:50:30 +0200

    liskov

---
+++ ProjectPatterns/SOLID/Liskov- example of broken impelentation/Square.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ProjectPatterns.SOLID.Liskov
+{
+    public class Square : Rectangle
+    {
+        public Square(int side)
+        {
+            Width = Height = side;
+        }
+
+        public new int Width
+        {
+            set { base.Width = base.Height = value; }
+        }
+
+        public new int Height
+        {
+            set { base.Width = base.Height = value; }
+        }
+
+        public static void UseIt(Rectangle rectangle)
+        {
+            rectangle.Height = 10;
+            Console.WriteLine($"Pole wynosi {10*rectangle.Width}, rzeczywiste {rectangle.Area}");
+        }
+    }
+}
